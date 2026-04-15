17a9dfdb9750e26f0e160972a82a77a71b38bf43
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2026-04-15 12:58:37 +0700

    membuat opsi ke masing masing menu

---
+++ Main.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Windows.Forms;
+
+namespace SaldoGo
+{
+    public partial class Main : Form
+    {
+        private UserSession session;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public Main(UserSession session) : this()
+        {
+            this.session = session;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Main_Shown(object sender, EventArgs e)
+        {
+            if (session == null)
+            {
+                MessageBox.Show("Session kosong.");
+                Close();
+                return;
+            }
+
+            lblLoginInfo.Text = "Login: " + session.FullName;
+
+            btnRiwayat.Enabled = true;
+
+            btnMenu.Enabled = session.IsOwner;
+            btnStock.Enabled = session.IsOwner;
+            btnTarget.Enabled = session.IsOwner;
+            btnKas.Enabled = session.IsOwner;
+            btnTransfer.Enabled = session.IsOwner;
+            btnMargin.Enabled = session.IsOwner;
+
+            lblNote.Visible = false;
+        }
+
+        private void btnExpense_Click(object sender, EventArgs e)
+        {
+            Transaksi f = new Transaksi(session);
+            f.ShowDialog(this);
+            f.Dispose();
+        }
+
+        private void btnRiwayat_Click(object sender, EventArgs e)
+        {
+            RiwayatBon f = new RiwayatBon(session);
+            f.ShowDialog(this);
+            f.Dispose();
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            Menu f = new Menu(session);
+            f.ShowDialog(this);
+            f.Dispose();
+        }
+
+        private void btnKas_Click(object sender, EventArgs e)
+        {
+            AkunKas f = new AkunKas(session);
+            f.ShowDialog(this);
+            f.Dispose();
+        }
+
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            Transfer f = new Transfer(session);
+            f.ShowDialog(this);
+            f.Dispose();
+        }
+
+        private void btnStock_Click(object sender, EventArgs e)
+        {
+            StokBahan f = new StokBahan(session);
+            f.ShowDialog(this);
+            f.Dispose();
+        }
+
+        private void btnTarget_Click(object sender, EventArgs e)
+        {
+            TargetOmzet f = new TargetOmzet(session);
+            f.ShowDialog(this);
+            f.Dispose();
+        }
+
+        private void btnMargin_Click(object sender, EventArgs e)
+        {
+            LaporanMargin f = new LaporanMargin(session);
+            f.ShowDialog(this);
+            f.Dispose();
+        }
+    }
+}
