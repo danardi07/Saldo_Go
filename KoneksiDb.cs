c904bd12061428fd7f10bc21954340f969b06c33
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2026-04-15 12:52:57 +0700

    membuat koneksi ke database yang telah dibuat di ssms

---
+++ KoneksiDb.cs
@@ -0,0 +1,8 @@
+using System;
+namespace SaldoGo
+{
+    internal static class KoneksiDb
+    {
+        public static string koneksi = @"Data Source=NAISE7; Initial Catalog=Saldo_Go;Integrated Security=True";
+    }
+}
