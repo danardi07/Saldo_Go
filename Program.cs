0496add103d19ed32117f45aa36ee3461855d061
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2026-04-15 13:02:43 +0700

    membuat program cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace SaldoGo
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new StatusKoneksi());
+        }
+    }
+}
